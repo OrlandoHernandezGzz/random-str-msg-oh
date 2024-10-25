5bb2f4d1138cf030e5a2d2254cd7612a282d89cf
Orlando <orlahdz50@gmail.com>
2024-10-25 15:21:57 -0600

    [ADD] Here be Dragons

---
+++ bin/global.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+let random = require('../src/index.js');
+
+random.funnyCommit();
+
+/**
+ * Genera una instrucción para indicarle que el archivo
+ * va a funcionar del lado de node, así como hacer un import
+ * del modulo que acabamos de crear y luego ejecuta la acción
+ */
